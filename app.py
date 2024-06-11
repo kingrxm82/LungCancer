11874e52f2bda60aca6b69c43234ea04c0406680
Ramzi Mejri <kingrxm82@gmail.com>
2024-06-11 23:12:27 +0100

    Add login and register

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/prediction')
 def prediction():
     return render_template('prediction.html')
